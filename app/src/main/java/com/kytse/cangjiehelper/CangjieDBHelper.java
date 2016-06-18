3ce65c7bf7a5e03565b315ddc005ebd8242cd947
Tse Kit Yam <me@kytse.com>
2016-06-19 02:32:09 +0800

    update copyright of all files

--- app/src/main/java/com/kytse/cangjiehelper/CangjieDBHelper.java
+++ app/src/main/java/com/kytse/cangjiehelper/CangjieDBHelper.java
@@ -1,4 +1,5 @@
-/* Copyright 2015 Tse Kit Yam
+/*
+ * Copyright 2016 Tse Kit Yam
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
